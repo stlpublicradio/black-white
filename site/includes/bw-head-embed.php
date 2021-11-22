63aa762b088173dc0c6fed7e94c2a99d6c474e65
Alexander Rice <webdev@ricealexander.com>
2021-11-22 12:29:02 -0600

    :recycle: Tidy up head embed
    - :fire: Remove Footer band-aid fix
    - :recycle: Replace GTM/Chartbeat script to exactly match the STLPR site
    - :abc: Slightly reorder assets

--- site/includes/bw-head-embed.php
+++ site/includes/bw-head-embed.php
@@ -1,24 +1,29 @@
-  <script><?php /* Google Tag Manager */ ?>
-  (function(w,d,s,l,i){
-    w[l]=w[l]||[];
-    w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
-    var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),
-        dl=l!='dataLayer'?'&l='+l:'';
-    j.async=true;
-    j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-    f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-KLPKLWL');
-  </script>
+  <script>
+  // Google Tag Manager
+  ;(function (clientID) {
+    window.dataLayer = window.dataLayer || []
+    window.dataLayer.push({
+      'gtm.start': (new Date()).getTime(),
+      event: 'gtm.js'
+    })
 
-  <script><?php /* NPR's (Chartbeat / Google Analytics) */ ?>
-  (function() {
-    var ga = document.createElement('script');
-    ga.async = true;
-    ga.src = '//stream.publicbroadcasting.net/analytics/aalp.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ga, s);
-  })();
+    var script = document.createElement('script')
+    script.async = true
+    script.src = 'https://www.googletagmanager.com/gtm.js?id=' + clientID
+
+    var firstScript = document.querySelector('script')
+    firstScript.parentNode.insertBefore(script, firstScript)
+  })('GTM-KLPKLWL')
+
+  // NPR's (Chartbeat / Google Analytics)
+  ;(function () {
+    var script = document.createElement('script')
+    script.async = true
+    script.src = '//stream.publicbroadcasting.net/analytics/aalp.js'
+
+    var firstScript = document.querySelector('script')
+    firstScript.parentNode.insertBefore(script, firstScript)
+  })()
   </script>
 
   <meta http-equiv="content-type" content="text/html; charset=utf-8">
@@ -27,18 +32,11 @@
   <link rel="shortcut icon" href="/favicon.ico">
 
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
   <link rel="stylesheet" href="/assets/bootstrap-theme.min.css">
-  <link rel="stylesheet" href="/assets/css/footer.css">
   <link rel="stylesheet" href="/assets/stlblackwhite.css">
+  <link rel="stylesheet" href="/assets/css/footer.css">
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-<script>
-$(function(){ // convert footer to Bootstrap 3
-  $('#umsl').removeClass().addClass('col-md-2');
-  $('#footernav').removeClass().addClass('col-md-10');
-});
-</script>
