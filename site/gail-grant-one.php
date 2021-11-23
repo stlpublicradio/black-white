6a49e745f85199e0cd1e8a265545031b81832cbe
Alexander Rice <webdev@ricealexander.com>
2021-11-22 21:12:12 -0600

    :speech_balloon: Embed Head onto all pages
    hard-code head embed onto all pages and remove PHP embed for it.

--- site/gail-grant-one.php
+++ site/gail-grant-one.php
@@ -2,7 +2,48 @@
 <html>
 <head>
   <title>Gail Milissa Grant: One Family's Experiencet</title>
-<?php include($_SERVER['DOCUMENT_ROOT'].'/includes/bw-head-embed.php');?>
+
+  <script>
+  // Google Tag Manager
+  ;(function (clientID) {
+    window.dataLayer = window.dataLayer || []
+    window.dataLayer.push({
+      'gtm.start': (new Date()).getTime(),
+      event: 'gtm.js'
+    })
+
+    var script = document.createElement('script')
+    script.async = true
+    script.src = 'https://www.googletagmanager.com/gtm.js?id=' + clientID
+
+    var firstScript = document.querySelector('script')
+    firstScript.parentNode.insertBefore(script, firstScript)
+  })('GTM-KLPKLWL')
+
+  // NPR's (Chartbeat / Google Analytics)
+  ;(function () {
+    var script = document.createElement('script')
+    script.async = true
+    script.src = '//stream.publicbroadcasting.net/analytics/aalp.js'
+
+    var firstScript = document.querySelector('script')
+    firstScript.parentNode.insertBefore(script, firstScript)
+  })()
+  </script>
+
+  <meta http-equiv="content-type" content="text/html; charset=utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" href="/favicon.ico">
+
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+  <link rel="stylesheet" href="/assets/css/bootstrap-theme.min.css">
+  <link rel="stylesheet" href="/assets/css/black-white.css">
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
 
