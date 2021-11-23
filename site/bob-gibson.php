a0411a419815c85e49f00a79b8452fab43fbcb75
Alexander Rice <webdev@ricealexander.com>
2021-11-22 20:54:55 -0600

    :speech_balloon: Embed Header onto all pages
    hard-code header onto all pages and remove PHP embed for it.

--- site/bob-gibson.php
+++ site/bob-gibson.php
@@ -4,9 +4,60 @@
   <title>Bob Gibson</title>
 <?php include($_SERVER['DOCUMENT_ROOT'].'/includes/bw-head-embed.php');?>
 </head>
-
 <body>
-<?php include($_SERVER['DOCUMENT_ROOT'].'/includes/bw-header.php');?>
+
+<header class="row stlbw-head-include">
+  <section id="stlbw-header">
+    <div class="redbar">
+      <div class="barcontent">
+        <ul>
+          <li id="bar-logo">
+            <a href="https://stlpr.org">
+              <img
+                src="https://stlpr.org/logos/stlpr-white"
+                alt="St. Louis Public Radio"
+                style="height: 27px; width: auto; margin-right: 22px;"
+              >
+            </a>
+          </li>
+          <li id="listen"><a href="https://news.stlpublicradio.org/listen" target="_blank"><i class="fa fa-volume-up"></i> Listen Live</a></li>
+          <li id="donate"><a href="https://stlpr.org/donate" target="_blank">Donate</a></li>
+        </ul>
+      </div>
+    </div>
+  </section>
+
+  <nav class="nav-wrapper hidden-xs" id="menu-pills">
+    <div class="container">
+      <ul class="nav nav-pills nav-justified">
+        <li role="presentation" style="margin-top: 2px"><a href="index.php">Home</a></li>
+        <li role="presentation"><a href="people.php">People</a></li>
+        <li role="presentation"><a href="events.php">Events</a></li>
+        <li role="presentation"><a href="experiences.php">Experiences</a></li>
+        <li role="presentation"><a href="ferguson.php">Ferguson</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="nav-wrapper hidden-sm hidden-md hidden-lg hidden-xl" id="menu-pills">
+    <div class="container">
+      <div class="dropdown" style="margin-left:30px;">
+        <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown" id="menu1"><i class="fa fa-bars" aria-hidden="true"></i> Menu</button>
+        <ul class="dropdown-menu" aria-labelledby="menu1">
+          <li role="presentation"><a role="menuitem" href="index.php">Home</a></li>
+          <li role="presentation" class="divider"></li>
+          <li role="presentation"><a role="menuitem" href="people.php">People</a></li>
+          <li role="presentation" class="divider"></li>
+          <li role="presentation"><a role="menuitem" href="events.php">Events</a></li>
+          <li role="presentation" class="divider"></li>
+          <li role="presentation"><a role="menuitem" href="experiences.php">Experiences</a></li>
+          <li role="presentation" class="divider"></li>
+          <li role="presentation"><a role="menuitem" href="ferguson.php">Ferguson</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</header>
 
 
 <main class="container">
